e821b430deac56bac230201872166a5816a33273
Tom Bilbe <tbilbe@gmail.com>
2017-11-04 16:00:04 +0000

    user mesage linked

---
+++ vacation.js
@@ -0,0 +1,8 @@
+// var user = window.prompt('Hi welcome to the VayKay Planner - Whats your name?!');
+// var greet = document.getElementById('greeting');
+// greet.innerHTML += ", " + user;
+
+
+var name = window.prompt("Hello! Please enter your name");
+var greeting = document.getElementById("greeting");
+greeting.innerHTML += ", " + name;
